2a16644be47df6243f219fae0b67dd446431f42a
黄世光 <1462174075@qq.com>
2018-03-23 09:57:29 +0800

    替换文件

--- Example/HSGBase/ViewController.swift
+++ Example/HSGBase/ViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import HSGBase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let myfileTest = MyFileTest()
+        
+        print(myfileTest.test())
+        print("●全新壹佳客与您见面，新的页面，新的体验。\r\n●页面有介绍，操作有引导，功能更强大，体验更友好。\r\n●萌宠“小壹”上线，您的贷款周期全程有它陪伴。")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ HSGBase/Classes/MyFileTest.swift
@@ -0,0 +1,14 @@
+//
+//  MyFileTest.swift
+//  Pods
+//
+//  Created by 黄世光 on 2018/3/21.
+//
+
+import Foundation
+
+public class MyFileTest: NSObject {
+    public func test() -> String{
+        return "success!"
+    }
+}
